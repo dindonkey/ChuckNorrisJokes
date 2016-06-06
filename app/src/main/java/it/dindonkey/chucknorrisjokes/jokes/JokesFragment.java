0b469db91014ec1419be2ade1686fbb686825c67
SimoneC <simone.caldon@trovaprezzi.it>
2016-06-06 14:44:39 +0200

    added jokes recycler view with adapter and view holder

--- app/src/main/java/it/dindonkey/chucknorrisjokes/jokes/JokesFragment.java
+++ app/src/main/java/it/dindonkey/chucknorrisjokes/jokes/JokesFragment.java
@@ -1,25 +1,93 @@
 package it.dindonkey.chucknorrisjokes.jokes;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import it.dindonkey.chucknorrisjokes.R;
+import it.dindonkey.chucknorrisjokes.data.Joke;
 
 public class JokesFragment extends Fragment
 {
+    private JokesAdapter mJokesAdapter;
+
     public static JokesFragment newInstance()
     {
         return new JokesFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        mJokesAdapter = new JokesAdapter(new ArrayList<Joke>(0));
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_jokes, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_jokes, container, false);
+
+        RecyclerView jokesRecyclerView = (RecyclerView) rootView.findViewById(R.id.jokes_list);
+        jokesRecyclerView.setAdapter(mJokesAdapter);
+        jokesRecyclerView.setHasFixedSize(true);
+        jokesRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
+        return rootView;
+    }
+
+    class JokesAdapter extends RecyclerView.Adapter<JokesAdapter.ViewHolder>
+    {
+        private final List<Joke> mJokes;
+
+        public JokesAdapter(List<Joke> jokes)
+        {
+            mJokes = jokes;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            Context context = parent.getContext();
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View jokeView = inflater.inflate(R.layout.item_joke, parent, false);
+
+            return new ViewHolder(jokeView);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position)
+        {
+            Joke joke = mJokes.get(position);
+            holder.jokeTextView.setText(joke.joke);
+        }
+
+        @Override
+        public int getItemCount()
+        {
+            return mJokes.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder
+        {
+            private final TextView jokeTextView;
+
+            public ViewHolder(View itemView)
+            {
+                super(itemView);
+                jokeTextView = (TextView) itemView.findViewById(R.id.joke_text);
+            }
+        }
     }
 }
